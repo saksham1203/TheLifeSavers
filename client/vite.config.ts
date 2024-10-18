51747525377435637ac734dc4c9de86613c5badd
saksham1203 <saksham.s1203@gmail.com>
2024-10-18 22:57:58 +0530

    update vercel files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure the output directory matches your Vercel config
+    outDir: 'dist',  // Ensure the output directory matches Vercel config
   },
   server: {
     port: 3000,  // Optional: Customize local dev server port
   },
   preview: {
-    port: 4173,  // Optional: Customize preview port
+    port: 4173,  // Optional: Customize preview server port
   },
 });
