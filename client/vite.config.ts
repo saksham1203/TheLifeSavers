c1ed62d4c6002f9042fb628d1ae4cb9316b2bf32
saksham1203 <saksham.s1203@gmail.com>
2024-10-18 21:32:33 +0530

    fixing revert

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures Vite knows how to resolve static assets
-  build: {
-    outDir: 'dist', // Vite output directory, ensure it's named 'dist'
-  },
-});
+})
